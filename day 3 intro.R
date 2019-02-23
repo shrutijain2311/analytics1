204538058518d00c0145de0769bc963b1d115f20
shrutijain2311 <shruti.2311jain@gmail.com>
2019-02-23 16:13:57 +0530

    q

---
+++ day 3 intro.R
@@ -0,0 +1,9 @@
+#what is decision tree?
+#difference between decision tree and machine learning?
+#decision trees are like if else
+#net r pakcage
+#CART- classification and regression tree
+#in decison tree which variable to split on (choosing) [gigi index,chi square,entropy]
+#entropy basically means chaos,higher the entropy higher the chaos
+
+
